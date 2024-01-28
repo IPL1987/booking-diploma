69f323e8fa7ab4afc2c324fbcb50c9905d939863
IPL1987 <ihorpoznyak@yandex.com>
2024-01-28 13:30:46 +0300

    configuring

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,23 +5,22 @@ import { UserModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { HotelsController } from './hotels/hotels.controller';
-import { HotelsService } from './hotels/hotels.service';
-import { HotelsModule } from './hotels/hotels.module';
-import { ChatModule } from './chat/chat.module';
+import { HotelModule } from './hotels/hotels.module';
 import { ReservationService } from './reservation/reservation.service';
 import { ReservationController } from './reservation/reservation.controller';
 import { ReservationModule } from './reservation/reservation.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
   imports: [
     UserModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/1'),
+    MongooseModule.forRoot('mongodb://localhost:27017/'),
     AuthModule,
-    HotelsModule,
+    HotelModule,
     ChatModule,
     ReservationModule,
   ],
   controllers: [AppController, HotelsController, ReservationController],
-  providers: [AppService, HotelsService, ReservationService],
+  providers: [AppService, ReservationService],
 })
 export class AppModule {}
