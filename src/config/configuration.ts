8a9bd39db4840df51f8434d4667be96a9685f586
IPL1987 <ihorpoznyak@yandex.com>
2024-02-05 12:35:44 +0300

    fix

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,14 +1,17 @@
 export default () => ({
   port: parseInt(process.env.PORT, 10) || 3000,
   database: {
-    host: process.env.DATABASE_HOST || 'localhost',
-    port: parseInt(process.env.DATABASE_PORT, 10) || 27017,
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT, 10) || 27017,
+    user: process.env.MONGO_USER || '',
+    password: process.env.MONGO_PASSWORD || '',
+    dbName: process.env.MONGO_DB_NAME || '',
   },
   cookie: {
     expires: process.env.COOKIE_EXPIRATION || 1000 * 36000,
   },
   jwt: {
-    secret: process.env.JWT_SECRET,
-    expiresIn: process.env.JWT_EXPIRATION_TIME || '7d',
+    secret: (process.env.JWT_SECRET = 'jwt'),
+    expiresIn: process.env.JWT_EXPIRATION || '7d',
   },
 });
