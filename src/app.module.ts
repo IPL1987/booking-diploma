b0abf47785815786f6b451b5c5f295f1c7fce60a
IPL1987 <ihorpoznyak@yandex.com>
2024-02-12 17:26:26 +0300

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,30 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AuthModule } from './auth/auth.module';
-import { HotelsController } from './hotels/hotels.controller';
 import { HotelModule } from './hotels/hotels.module';
-import { ReservationService } from './reservation/reservation.service';
-import { ReservationController } from './reservation/reservation.controller';
-import { ReservationModule } from './reservation/reservation.module';
 import { ChatModule } from './chat/chat.module';
 import { ConfigModule } from '@nestjs/config';
 import configuration from './config/configuration';
+import { ReservationModule } from './reservation/reservation.module';
 
 @Module({
   imports: [
-    UserModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/'),
-    AuthModule,
-    HotelModule,
-    ChatModule,
-    ReservationModule,
     ConfigModule.forRoot({
       isGlobal: true,
       load: [configuration],
     }),
+    UserModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/'),
+    ReservationModule,
+    HotelModule,
+    ChatModule,
   ],
-  controllers: [AppController, HotelsController, ReservationController],
-  providers: [AppService, ReservationService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
